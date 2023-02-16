93ed135481b69b2c2103e6b66c1edb8b6f66c568
Jingying Wang <wangchy@umich.edu>
2023-02-16 16:07:21 -0500

    Update __init__.py

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -1,1 +1,30 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Tianheng Cheng(tianhengcheng@gmail.com)
+# ------------------------------------------------------------------------------
+
+from .aflw import AFLW
+from .cofw import COFW
+from .face300w import Face300W
+from .custom import CUSTOM
+from .wflw import WFLW
+
+__all__ = ['AFLW', 'COFW', 'Face300W', 'WFLW', 'get_dataset']
+
+
+def get_dataset(config):
+
+    if config.DATASET.DATASET == 'AFLW':
+        return AFLW
+    elif config.DATASET.DATASET == 'COFW':
+        return COFW
+    elif config.DATASET.DATASET == '300W':
+        return Face300W
+    elif config.DATASET.DATASET == 'WFLW':
+        return WFLW
+    elif config.DATASET.DATASET == 'CUSTOM':
+        return CUSTOM
+    else:
+        raise NotImplemented()
 
